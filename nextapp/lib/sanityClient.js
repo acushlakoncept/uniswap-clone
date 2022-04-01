27ff0e75105aa8ce1ed75e36a79772e79d7cdc50
Uduak Essien <acushla4real@gmail.com>
2022-04-01 21:22:21 +0100

    Add sanity token to .env file

--- nextapp/lib/sanityClient.js
+++ nextapp/lib/sanityClient.js
@@ -1,9 +1,9 @@
 import sanityClient from '@sanity/client';
 
 export const client = sanityClient({
-  projectId: '51ialwqd',
+  projectId: process.env.SANITY_APP_ID,
   dataset: 'production',
   apiVersion: 'v1',
-  token: 'skXBiBRVc4s1joW3qQ13KKj75wKhftDuGHktWjH5dWZxjkh7Rh7mqt2WtMznVefffEQw8TrdfxHqdKd1lMgBUXJuMYRiyyzcD9C5fbbysGy3vDp9OkuVKlINpvSvJCh3ueHA4lc3R2tCA9oG8wf5jP6ON4s8RMrCOGgc8Bg9KYMKX732dIqg',
+  token: process.env.SANITY_TOKEN,
   useCdn: false,
 })
--- nextapp/next.config.js
+++ nextapp/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  env: {
+    SANITY_APP_ID: process.env.SANITY_APP_ID,
+    SANITY_TOKEN: process.env.SANITY_TOKEN,
+  }
 }
