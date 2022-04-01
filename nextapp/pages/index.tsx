52d7420920bfe177fa55ee60e4ce0cb41a66d658
Uduak Essien <acushla4real@gmail.com>
2022-04-01 10:26:37 +0100

    Update header and main

---
+++ nextapp/pages/index.tsx
@@ -0,0 +1,20 @@
+
+import Header from '../components/header'
+import Main from '../components/main'
+
+const style = {
+  wrapper: 'h-screen max-h-screen h-min-screen w-screen bg-[#2D242F] text-white select-none flex flex-col justify-between',
+}
+
+const Home = () => {
+
+  return (
+    <div className={style.wrapper}>
+      <Header />
+      <Main />
+      <h2>Transaction</h2>
+    </div>
+  )
+}
+
+export default Home
